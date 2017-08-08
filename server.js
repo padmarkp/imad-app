2f139373b45ec45f8226c01f24fac56c6466a7b7
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-08 19:45:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/', function (req, res) {
 
 app.get('/articleName', function (req, res) {
     var articleName=req.params.articleName;
-    res.send(creteTemplate(articles[articleName]));
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
