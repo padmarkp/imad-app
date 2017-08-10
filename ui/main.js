8556abeea632abb2140ac90516ddee092654d163
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-10 20:40:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,14 +28,14 @@ button.onclick = function()
         
         if (request.status === 200) 
         {
-         var counter=request.responseTest;
+         var counter=request.responseText;
          var span = document.getElementById('count');
          span.innerHTML= counter.toString();
         }
         
     };
     
-    request.open('GET' , 'http://padmarkp.imad.hasura-app.io/counter', true);
+    request.open("GET", 'http://padmarkp.imad.hasura-app.io/counter', true);
     request.send(null);
     
     
