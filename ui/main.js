d47d1331e962db26260813c8b1e3026b21ebf6c9
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-10 14:22:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,15 @@ console.log('Loaded!');
 var element= document.getElementById('main-text');
 element.innerHTML="new value";
 
-var img = document.getElementById('madi-img');
-
+var img = document.getElementById('madi');
+var marginLeft=0;
+function moveLeft()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft= marginLeft+'px';
+}
 img.onclick = function ()
 {
-    img.style.marginLeft='50px';
-    
+    //img.style.marginLeft='50px';
+    var interval= setInterval(moveLeft , 100 );
 };
