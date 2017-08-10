1ecba178f5ba31ed86c901fbe401ba6b5ad50d72
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-10 19:05:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,20 @@
 console.log('Loaded!');
 // change the text 
-var element= document.getElementById('main-text');
-element.innerHTML="new value";
+//var element= document.getElementById('main-text');
+//element.innerHTML="new value";
 
-var img = document.getElementById('madi');
-var marginRight=0;
-function moveRight()
-{
-    marginRight=marginRight+1;
-    img.style.marginRight= marginRight+'px';
-}
-img.onclick = function ()
-{
-    //img.style.marginLeft='50px';
-    var interval= setInterval(moveRight , 5 );
-};
+//var img = document.getElementById('madi');
+//var marginRight=0;
+//function moveRight()
+//{
+//    marginRight=marginRight+1;
+//    img.style.marginRight= marginRight+'px';
+//}
+//img.onclick = function ()
+//{
+//    //img.style.marginLeft='50px';
+//    var interval= setInterval(moveRight , 5 );
+//};
 var counter=0;
 var button= document.getElementById('counter');
 button.onclick = function()
