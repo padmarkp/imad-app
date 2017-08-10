28df7b2f3f3b8e15629a1ba1191dc7f77d9dd5c0
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-10 13:41:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,11 @@ console.log('Loaded!');
 // change the text 
 var element= document.getElementById('main-text');
 element.innerHTML="new value";
+
+var img = document.getElementById('madi-img');
+
+img.onclick = function ()
+{
+    img.style.marginLeft='100px';
+    
+};
