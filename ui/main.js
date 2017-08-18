b68c66f01c41626b706cd7e7d8e2a0ce5b3baa7e
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-18 13:44:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,3 +40,19 @@ button.onclick = function()
     
     
 };
+var nameInput= document.getElementById('name');
+var name = nameInput.value;
+var submit= document.getElementById('submit_btn');
+submit.onclick = function()
+{
+    var names= ['name1', 'name2', 'name3', 'name4'];
+    var list='';
+    for(var i=0; i< name.lenght ; i++)
+    {
+    list +='<li>' + names[i] + '</li>';
+    
+     }
+var ul= document.getElementById('namelist');
+ul.innerhtml =list;
+
+};
