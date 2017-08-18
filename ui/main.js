f336cf4898b3346d7cf59586d8be26b428625bb5
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-18 13:17:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ button.onclick = function()
         if (request.status === 200) 
         {
          var counter=request.responseText;
-         var span = document.getElementById('count');
+         var span = document.getElementById('counter');
          span.innerHTML= counter.toString();
         }
         }
