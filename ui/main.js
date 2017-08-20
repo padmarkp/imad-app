5b63a1271cb32b7c88e5b0ee6217772613cc9b82
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-20 16:29:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,10 +2,19 @@ var button= document.getElementById('counter');
 var counter=0;
 button.onclick = function()
 {
-    counter= counter + 1;
-    var span=document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
+    var request = new XMLHttpRequest();
+    request.onreadyStateChange =  function()
+    {
+        if (request.readystate === XMLHttpRequest.DONE);
+        {
+        if (request.status === 200) 
+        {
+            var counter = request.responseText();
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
+        }
+        }
+    }
 };
 
 var submit= document.getElementById('submit_btn');
