0f712257662e35f5c77a52b9e0178ade460c979e
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-20 15:16:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,26 +17,12 @@
 //};
 
 var button= document.getElementById('counter');
-
+var counter=0;
 button.onclick = function()
 {
-    var request = new XMLHttpRequest();
-    request.onreadyStateChange =  function()
-    {
-        if (request.readystate === XMLHttpRequest.DONE)
-        {
-        if (request.status === 200) 
-        {
-         var counter=request.responseText;
-         var span = document.getElementById('counter');
-         span.innerHTML= counter.toString();
-        }
-        }
-    };
-    
-    request.open('GET', 'http://padmarkp.imad.hasura-app.io/counter', true);
-    request.send(null);
-    
+    counter= counter + 1;
+    var span=document.getElementById('count');
+    span.innerhtml= counter.toString();
     
 };
 
