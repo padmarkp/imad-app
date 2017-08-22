712984cd2542f1fab14c59f45d8b4bb72c7076e5
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-22 14:11:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,12 +128,12 @@ app.get('/test-db', function(req,res){
 });
 
 
-var counter=0;
+/*var counter=0;
 app.get('/counter',  function(req, res){
     counter = counter + 1;
     res.send(counter.toString());
 });
-
+*/
 
 //to extract a bit of string usng parameter
 
