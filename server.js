f82ecf0b9c4546437553e49e8f8563153c8ab3e1
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-22 14:09:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,45 +139,29 @@ app.get('/counter',  function(req, res){
 
 //to extract a bit of string using query parameter(? just change params to query)
 
-var names = [];
+/*var names = [];
 app.get ('/submit-name', function(req, res){
     var name = req.query.name;
     
     names.push(name);
     res.send(JSON.stringify(names));//to convert array into string
 });
+*/
 
-
-
-
-
-
-
-app.get('/articles/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res)
+{
 var articleName=req.params.articleName;
-
-
 pool.query("SELECT * FROM article WHERE title='"+ req.params.articleName + "' ", function(err,result){
- 
 if(err){
-
 res.status(500).send(err.toString());
     }
-
 else
-
     {
-
         if(result.rows.length===0)
-
         {res.status(404).send('article not found');
- 
        }
-
         else
-
         {
-
             var articleData = result.rows[0];
              res.send(createTemplate(articles[articleData]));
        }
