531d9dfd40c06492ce2f8f308a115a6400b5c82f
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-22 13:14:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config={
     database: 'padmarkp',
     host:'db.imad.hasura.app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD
 };
 
 
@@ -113,10 +113,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-/*var pool= new Pool(config);
+var pool= new Pool(config);
 
 app.get('/test-db', function(req,res){
-    pool.query("SELECT * FROM test" , function(err, result){
+    pool.query('SELECT * FROM test' , function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }
@@ -127,7 +127,7 @@ app.get('/test-db', function(req,res){
     });
 });
 
-*/
+
 var counter=0;
 app.get('/counter',  function(req, res){
     counter = counter + 1;
