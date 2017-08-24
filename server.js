7574a479a4ce284b60092bf8aafbb1d26bb129b7
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-24 15:46:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ app.get ('/submit-name', function(req, res){
 
 app.get('/articles/:articleName', function (req, res)/*/: sending data as part of the url*/
 {
-pool.query("SELECT * FROM article WHERE title= "+ [req.params.articleName], function(err,result){
+pool.query("SELECT * FROM article WHERE title= '"+ req.params.articleName +"'", function(err,result){
 if(err){
 res.status(500).send(err.toString());
     }
