f4aaf8b1204e007c10b7d61b4d001c9bcec3c3de
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-24 23:32:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto= require('crypto');
-var bodyParser= require('body-Parser');
+var bodyParser= require('body-parser');
 
 
 var config={
@@ -135,7 +135,7 @@ app.post('/create-user',function(req,res)
     var username= req.body.username;
     var password= req.body.password;
     var salt= crypto.randomBytes(128).toString('hex');
-    var dbString = hash(password, salt);
+    var dbString = hash(password,salt);
     
     pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username,dbString], function(err, result){
         
