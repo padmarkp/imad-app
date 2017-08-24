26cce8d52304f9339d007fe8aa68d4f926f57adc
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-24 19:22:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var app = express();
+var crypto= require('crypto');
 app.use(morgan('combined'));
 
 var config={
@@ -111,6 +112,18 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input, salt)
+{
+    var hashed= crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req, res){
+   var hashedString =hash(req.params.input,'this is some random string');
+   res.send(hashedString);
+});
+
 
 var pool= new Pool(config);
 /*18/08/17: Testconnection to database: this part is working */
