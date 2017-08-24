999c385b073233db07d98a28456840f036335d9b
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-24 23:43:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -199,26 +199,21 @@ app.get ('/submit-name', function(req, res){
 });
 */
 
-app.get('/articles/:articleName', function (req, res)/*/: sending data as part of the url*/
-{
-pool.query("SELECT * FROM article WHERE title= '"+ req.params.articleName +"'", function(err,result){
-if(err){
-res.status(500).send(err.toString());
-    }
-else
-    {
-        if(result.rows.length === 0)
-        {res.status(404).send('article not found');
-       }
-        else
-        {
+app.get('/articles/:articleName', function (req, res) {
+  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
             var articleData = result.rows[0];
-             res.send(createTemplate(articles[articleData]));
-       }
+            res.send(createTemplate(articleData));
+        }
     }
+  });
 });
-});
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
