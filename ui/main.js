6e9e62bf752b7ecd465ab52ea037f31adb8a4b90
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-25 11:55:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var button= document.getElementById('counter');        //ModuleP6----------still
 button.onClick = function()
 {
             counter = counter+1;        
-            var span=document.getElementById('count');
+            var span=document.getElementById("count");
             span.innerHTML = counter.toString();
  };
 
