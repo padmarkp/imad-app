7093bdcda8e9b76982ce0674b7bd0c3b145d3ac3
Padmarkp <30745624+padmarkp@users.noreply.github.com>
2017-08-26 00:19:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,31 +20,39 @@ button.onclick = function()
 
 };
 
-
-var submit= document.getElementById('submit_btn');
-submit.onclick = function()
+var
+ Submit= document.getElementById('Submit_btn1');
+Submit.onclick = function()
 {
   var request = new XMLHttpRequest();
     request.onreadystatechange =  function()
     {
         if (request.readystate === XMLHttpRequest.DONE);
         {
-        if (request.status === 200) 
-        {
-         var names = request.responseText;
-         names = JSON.parse(names);  /* parse used toconvert string back into an array*/
-        var list='';
-    for(var i=0; i< name.length ; i++)
-    {
-    list +='<li>' + names[i] + '</li>';
+        if (request.status === 200){
+        alert("Logged in successfully");}
+        else if (request.status === 403){
+        alert("username/password is incorrect");}
+        else if (request.status === 500){
+        alert("Something went wrong on the server");
+        }
     }
-var ul= document.getElementById('namelist');
-ul.innerHTML = list;
-}
-}
+
+var username= document.getElementById('username').value;
+var password= document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST', "http://padmarkp.imad.hasura-app.io/login",true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({username: username, password: password}));
 };
-var nameInput= document.getElementById('name');
-var name = nameInput.value;
-request.open('GET', 'http://padmarkp.imad.hasura-app.io/submit-name?name=' + name, true);
-request.send(null);
+
+
+
+
+
+
+
+
+
 };
